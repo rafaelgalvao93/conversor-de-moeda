43a6e4d6d001506a03476784e1da077a9ec0bc65
rafaelgalvao93 <rafaelgalvao.rap@gmail.com>
2023-04-07 19:10:40 -0300

    commit-inlusao-bitcoin

--- scipts.js
+++ scipts.js
@@ -3,15 +3,12 @@ const select = document.getElementById('currency-select')
 
 const dolar = 5.2
 const euro = 5.9
+const bitcoin = 0.0000071
 
 const convertValues = () => {
     const inputReais = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
-
-    // realValueText.innerHTML = inputReais
-
-    //    currencyValueText.innerHTML = (inputReais / dolar)
 
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
         { style: 'currency', currency: 'BRL' }
@@ -30,6 +27,13 @@ const convertValues = () => {
 
     }
 
+    if (select.value === "Bitcoin") {
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'BTC' }
+        ).format(inputReais / bitcoin)
+
+    }
+    
 
 }
 
@@ -46,6 +50,11 @@ changeCurrency = () => {
     if (select.value === '€ Euro') {
         CurrencyName.innerHTML = "Euro"
         currencyImg.src = "./assets/euro.jpg"
+    }
+
+    if (select.value === 'Bitcoin') {
+        CurrencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "./assets/bitcoin.png"
     }
 
     convertValues()
